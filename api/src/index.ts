3a5c1c500d6fe7c0ac9bc1a0fe5f713afcad55db
MarkedOlive <olivdev4@gmail.com>
2023-03-03 20:07:40 -0400

    Updating Port on api to use react

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import server from './server';
 import './database'
 
-server.listen(3000, () => {
-    console.log('Server is running on PORT: 3000')
+server.listen(3001, () => {
+    console.log('Server is running on PORT: 3001')
 })
